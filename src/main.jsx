1ce358e7b338a4bcae47009f258a8a8d7822f1f3
Jack C <jackchu123@gmail.com>
2024-01-25 14:25:26 +1100

    configured react router for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
+        <BrowserRouter basename="/ecommerce-frontend" >
           <App />
         </BrowserRouter>
       </QueryClientProvider >
