43a2d87f02f0444caab405c7bd7246f3d1d23eb8
Jack C <jackchu123@gmail.com>
2024-01-29 18:10:36 +1100

    revert api address back

--- src/services/fetchProducts.js
+++ src/services/fetchProducts.js
@@ -1,7 +1,7 @@
 import { productsReplace, obtainCategories } from "../state/productSlice"
 import store from "../state/store"
 
-const PRODUCT_URL = "/api/products"
+const PRODUCT_URL = "http://ec2-34-229-171-36.compute-1.amazonaws.com/api/products"
 
 export async function fetchAllProducts() {
     const response = await fetch(`${PRODUCT_URL}/all`)
